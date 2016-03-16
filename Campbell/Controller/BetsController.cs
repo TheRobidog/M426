787e3eab0297d4432cf1b4b8f9ce583bbe969469
thomas-campbell <t.campbell7789@gmail.com>
2016-03-16 11:29:56 +0100

    meine code-abschnitten
    code 16.03 Campbell (betcontroller, bet model & migrations)

---
+++ Campbell/Controller/BetsController.cs
@@ -0,0 +1,19 @@
+﻿using System.Linq; 
+using System.Web.Mvc;
+using SportBet.Models;
+public class BetsController : Controller { 
+    private ApplicationDbContext _dbContext; 
+    public BetsController() { 
+        _dbContext = new ApplicationDbContext(); 
+    }
+    // GET: Videos
+    public ActionResult Index() {
+        var bets = _dbContext.Bets.ToList(); 
+        return View(bets); 
+    }
+
+    public ActionResult Create() {
+        return View(); 
+    }
+
+}
---
+++ Campbell/Models/Bet.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SportBet.Models
+{
+    public class Bet
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Odds { get; set; }
+        public string Amount { get; set; }
+    }
+}
